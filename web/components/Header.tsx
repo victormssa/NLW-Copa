7b080d8844d2fb42026cb712fc66c244b1a35b33
Victor Alves <victormssalves@outlook.com>
2022-11-02 16:33:38 -0300

    Ínicio do front

---
+++ web/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header(props: any) {
+    return (
+        <header>
+            <h1>{props.text}</h1>
+        </header>
+    );
+};
---
+++ web/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../components/Header";
+
+export default function Home() {
+  return (
+    <main>
+      <Header text='NLW Page'></Header>
+      <h1>Hello, NLW!</h1>
+    </main>
+  )
+}
