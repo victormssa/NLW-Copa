24fa596ac6f395d3666bd6417e2f9fe9f50b28be
Victor Alves <victormssalves@outlook.com>
2022-11-02 14:06:06 -0300

    Commit inicial

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import Fastify from "fastify";
+
+async function bootstrap() {
+    const fastify = Fastify({
+        logger: true,
+    });
+    
+    fastify.get('/pools/count', () => {
+        return { count: 713 }
+    });
+    
+    await fastify.listen({ port: 3333 });
+};
+bootstrap();
